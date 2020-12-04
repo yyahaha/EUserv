2e35cddfab6c25ddb3d42464b011996462f270e0
CokeMine <aptx4561@gmail.com>
2020-12-04 22:05:09 +0800

    v0.04

--- main.py
+++ main.py
@@ -130,6 +130,8 @@ if __name__ == "__main__":
             if v:
                 if not renew(sessid, s, passwd_list[i], k):
                     print("ServerID: %s Renew Error!" % k)
+                else:
+                    print("ServerID: %s has been successfully renewed!" % k)
             else:
                 print("ServerID: %s does not need to be renewed" % k)
         time.sleep(15)
